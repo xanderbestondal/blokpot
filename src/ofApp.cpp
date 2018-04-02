52fa4732954db34de67a1155396e1abf788d96b7
xander clerckx <xanderclerckx@hotmail.com>
2018-04-02 15:54:17 +0200

    innitial comittt

---
+++ src/ofApp.cpp
@@ -0,0 +1,103 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+	testShape.setup();
+	testShape.cylinder(5, 100, 400, 300, false, true);
+
+	//we need to call this for textures to work on models
+	//ofDisableArbTex();
+	//this makes sure that the back of the model doesn't show through the front
+	glEnable(GL_DEPTH_TEST);
+
+	ofSetWindowShape(1280, 800);
+
+	light.setPosition(1000, 1000, 1000);
+	light.enable();
+
+	testShape.loadObj("meshes/sam.obj");
+
+	//cam.begin();
+	//cam.setPosition(0, 0, 1000);
+	//cam.enableOrtho();
+	//testShape.mapPlannar(cam);
+	////testShape.setTexture(displaceImg);
+	//cam.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+	cam.begin();
+	cam.enableOrtho();
+
+	cam.setPosition(0, 0, 1000);
+
+	//ofRotate(45, 1, 1, 0);
+	testShape.show();
+	//testShape.mapByLight(light.getPosition());
+
+	cam.end();
+	testShape.vertPosToRgb();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
